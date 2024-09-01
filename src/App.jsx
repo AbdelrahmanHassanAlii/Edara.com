c88dd2580f45d46547b3fd19395071d5b81fba6a
venom <abdo30201080101694@gmail.com>
2024-09-01 18:16:36 +0300

    add the svgs and remove we didn't need

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
-
+import "./App.css";
+import screen from "./assets/SVG/screen.svg";
+import react from "./assets/SVG/react.svg";
+import laptop from "./assets/SVG/laptop.svg";
+import thinBar from "./assets/SVG/thin-bar-dark.svg";
 function App() {
   return (
     <>
+      <h1>Edara</h1>
+      <img src={react} alt="react" width={200} />
+      <img src={screen} alt="screen" width={200} />
+      <img src={laptop} alt="laptop" width={200} />
+      <img src={thinBar} alt="thinBar" width={200} />
       <div>App</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
