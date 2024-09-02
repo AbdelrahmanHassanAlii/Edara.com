693aa5f04796827b0f8aa30c7539ba408815956f
abdo30201080101694 <abdo30201080101694@gmail.com>
2024-09-02 13:51:51 +0000

    add data of login inputs

---
+++ src/Helper/Functions/loginInputs.js
@@ -0,0 +1,14 @@
+export const loginData = [
+    {
+        label: "User name",
+        name: "user_name",
+        type: "text",
+        placeHoolder: "Your user name"
+    },
+    {
+        label: "Password",
+        name: "password",
+        type: "password",
+        placeHoolder: "Your password"
+    }
+]
