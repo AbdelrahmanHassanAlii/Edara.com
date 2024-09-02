f0a47cd1369914e902033997aa1076b09942f163
venom <abdo30201080101694@gmail.com>
2024-09-03 00:28:48 +0300

    handle the basic logic of login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import "./App.css";
 
+import LoginForm from "./Components/Shared/LoginForm";
 import SignForm from "./Components/Shared/SignForm";
 
 function App() {
   return (
     <>
       <SignForm />
+      <LoginForm />
       <div>App</div>
     </>
   );
--- src/Components/Shared/LoginForm.jsx
+++ src/Components/Shared/LoginForm.jsx
@@ -1,8 +1,30 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { loginData } from './../../Helper/Functions/loginInputs';
+import Inputs from './Inputs';
 
 export default function LoginForm() {
+
+  const [loginInputs, setLoginInputs] = useState({
+    user_name: "",
+    password: ""
+  });
+
+  const [lodinData, setLoginData] = useState([]);
+
+  const handleChange = (e) => {
+    setLoginInputs({ ...loginInputs, [e.target.name]: e.target.value });
+    console.log(loginInputs);
+  };
+
+  useEffect(() => {
+    setLoginData(loginData);
+  }, []);
+
   return (
-    <div className='login-inputs'></div>
+    <div className='login-inputs'>
+
+      <Inputs inputs={lodinData} handleChange={handleChange} />
+    </div>
   )
 }
